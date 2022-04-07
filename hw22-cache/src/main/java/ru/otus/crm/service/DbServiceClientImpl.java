ba3d0b3911025c7106da6f0e2a7e6a207d7a46bb
shurupov <bolteg86@ya.ru>
2022-04-08 00:16:14 +0400

    22 Added cache

--- hw22-cache/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
+++ hw22-cache/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
@@ -2,6 +2,8 @@ package ru.otus.crm.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.otus.cachehw.HwCache;
+import ru.otus.cachehw.MyCache;
 import ru.otus.core.repository.DataTemplate;
 import ru.otus.core.sessionmanager.TransactionRunner;
 import ru.otus.crm.model.Client;
@@ -14,6 +16,7 @@ public class DbServiceClientImpl implements DBServiceClient {
 
     private final DataTemplate<Client> dataTemplate;
     private final TransactionRunner transactionRunner;
+    private final HwCache<Long, Client> cache = new MyCache<>();
 
     public DbServiceClientImpl(TransactionRunner transactionRunner, DataTemplate<Client> dataTemplate) {
         this.transactionRunner = transactionRunner;
@@ -27,19 +30,26 @@ public class DbServiceClientImpl implements DBServiceClient {
                 var clientId = dataTemplate.insert(connection, client);
                 var createdClient = new Client(clientId, client.getName());
                 log.info("created client: {}", createdClient);
+                cache.put(clientId, createdClient);
                 return createdClient;
             }
             dataTemplate.update(connection, client);
             log.info("updated client: {}", client);
+            cache.put(client.getId(), client);
             return client;
         });
     }
 
     @Override
     public Optional<Client> getClient(long id) {
+        Client cachedClient = cache.get(id);
+        if (cachedClient != null) {
+            return Optional.of(cachedClient);
+        }
         return transactionRunner.doInTransaction(connection -> {
             var clientOptional = dataTemplate.findById(connection, id);
             log.info("client: {}", clientOptional);
+            clientOptional.ifPresent(client -> cache.put(id, client));
             return clientOptional;
         });
     }
@@ -48,6 +58,7 @@ public class DbServiceClientImpl implements DBServiceClient {
     public List<Client> findAll() {
         return transactionRunner.doInTransaction(connection -> {
             var clientList = dataTemplate.findAll(connection);
+            clientList.forEach(c -> cache.put(c.getId(), c));
             log.info("clientList:{}", clientList);
             return clientList;
        });
--- hw22-cache/src/main/java/ru/otus/crm/service/DbServiceManagerImpl.java
+++ hw22-cache/src/main/java/ru/otus/crm/service/DbServiceManagerImpl.java
@@ -2,6 +2,8 @@ package ru.otus.crm.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.otus.cachehw.HwCache;
+import ru.otus.cachehw.MyCache;
 import ru.otus.core.repository.DataTemplate;
 import ru.otus.core.sessionmanager.TransactionRunner;
 import ru.otus.crm.model.Manager;
@@ -14,6 +16,7 @@ public class DbServiceManagerImpl implements DBServiceManager {
 
     private final DataTemplate<Manager> managerDataTemplate;
     private final TransactionRunner transactionRunner;
+    private final HwCache<Long, Manager> cache = new MyCache<>();
 
     public DbServiceManagerImpl(TransactionRunner transactionRunner, DataTemplate<Manager> managerDataTemplate) {
         this.transactionRunner = transactionRunner;
@@ -27,19 +30,26 @@ public class DbServiceManagerImpl implements DBServiceManager {
                 var managerNo = managerDataTemplate.insert(connection, manager);
                 var createdManager = new Manager(managerNo, manager.getLabel(), manager.getParam1());
                 log.info("created manager: {}", createdManager);
+                cache.put(managerNo, createdManager);
                 return createdManager;
             }
             managerDataTemplate.update(connection, manager);
             log.info("updated manager: {}", manager);
+            cache.put(manager.getNo(), manager);
             return manager;
         });
     }
 
     @Override
     public Optional<Manager> getManager(long no) {
+        Manager manager = cache.get(no);
+        if (manager != null) {
+            return Optional.of(manager);
+        }
         return transactionRunner.doInTransaction(connection -> {
             var managerOptional = managerDataTemplate.findById(connection, no);
             log.info("manager: {}", managerOptional);
+            managerOptional.ifPresent(m -> cache.put(m.getNo(), m));
             return managerOptional;
         });
     }
@@ -48,6 +58,7 @@ public class DbServiceManagerImpl implements DBServiceManager {
     public List<Manager> findAll() {
         return transactionRunner.doInTransaction(connection -> {
             var managerList = managerDataTemplate.findAll(connection);
+            managerList.forEach(m -> cache.put(m.getNo(), m));
             log.info("managerList:{}", managerList);
             return managerList;
        });
