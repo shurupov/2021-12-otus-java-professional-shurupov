78171ae732e2e290294d699102919db975167df1
shurupov <bolteg86@ya.ru>
2022-06-30 01:24:17 +0400

    hw39 Created secrets. Prepared the application to use database

--- hw39-coursework/homeowners/src/main/java/ru/shurupov/homeowners/core/controller/SimpleController.java
+++ hw39-coursework/homeowners/src/main/java/ru/shurupov/homeowners/core/controller/SimpleController.java
@@ -1,5 +1,6 @@
 package ru.shurupov.homeowners.core.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,24 @@ public class SimpleController {
 
     private static final Random random = new Random();
 
+    @Value("${some.variable}")
+    private String someValue;
+
+    @Value("${some.secret.variable}")
+    private String secretValue;
+
     @GetMapping("/api/try")
     public String getTry() {
         return "success " + random.nextInt();
+    }
+
+    @GetMapping("/api/variable")
+    public String getVariable() {
+        return someValue;
+    }
+
+    @GetMapping("/api/secret")
+    public String getSecret() {
+        return secretValue;
     }
 }
